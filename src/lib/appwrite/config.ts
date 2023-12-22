e4c0556683c064348bba7c1e624570f004240d90
Adarsh Jha <adarshjhaxif@gmail.com>
2023-12-22 21:18:38 +0530

    first commit

---
+++ src/lib/appwrite/config.ts
@@ -0,0 +1,21 @@
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
+
+export const appwriteConfig = {
+  projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  appwriteEndpoint: import.meta.env.VITE_APPWRITE_ENDPOINT,
+  databaseId: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  usersCollectionId: import.meta.env.VITE_APPWRITE_USERS_COLLECTION_ID,
+  postsCollectionId: import.meta.env.VITE_APPWRITE_POSTS_COLLECTION_ID,
+  savesCollectionId: import.meta.env.VITE_APPWRITE_SAVES_COLLECTION_ID,
+  storageId: import.meta.env.VITE_APPWRITE_STORAGE_ID,
+};
+
+export const client = new Client();
+
+client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.appwriteEndpoint);
+
+export const account = new Account(client);
+export const storage = new Storage(client);
+export const databases = new Databases(client);
+export const avatars = new Avatars(client);
